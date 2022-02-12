6042fd3a83c64de7bb8e4648c2bb58e4a615357e
q349980363 <759323585@qq.com>
2022-02-12 18:11:34 +0800

    今日修改
    Co-authored-by: lingy.io@qq.com <lingy.io@qq.com>

--- vue/src/hub.ts
+++ vue/src/hub.ts
@@ -1,5 +1,6 @@
-class Hub {
-  //   [key: string]: Promise<any>
+export class Hub {
+  ID = 0;
+  callBackList: { [key: string]: CallBack<any> } = {};
   websocket: WebSocket = <any>{};
   url: string;
   constructor(wsUri: string) {
@@ -20,26 +21,45 @@ class Hub {
     });
   }
 
-  // onConnection() {}
-  onError(e:Event) {
-    console.error("Hub onError",e)}
-  onClose(e:Event) {
-    console.error("Hub onclose",e)
+  onError(e: Event) {
+    console.error("Hub onError", e);
   }
 
+  onClose(e: Event) {
+    console.error("Hub onclose", e);
+  }
 
-
-  
   onMessage(methodName: string, newMethod: (...args: any[]) => void) {}
   on(methodName: string, newMethod: (...args: any[]) => void) {}
-  invoke<T = any>(methodName: string, ...args: any[]): Promise<T> {
-    //  new Promise();
 
-    return new Promise((resolve, reject) => {});
+  invoke<T = any>(
+    hubName: string,
+    methodName: string,
+    ...args: any[]
+  ): Promise<any> {
+    var ID = this.GetID;
+    var promise = new Promise<T>((resolve, reject) => {
+      this.callBackList[ID.toString()] = new CallBack<T>(resolve, reject);
+    });
+    return promise;
   }
+
+  GetID = () => this.ID++;
 }
 
-var hub = new Hub("");
-hub.on("a", (a: string) => {});
+
+
+
+class CallBack<T> {
+  resolve: (value: T | PromiseLike<T>) => void;
+  reject: (reason?: any) => void;
+  constructor(
+    resolve: (value: T | PromiseLike<T>) => void,
+    reject: (reason?: any) => void
+  ) {
+    this.resolve = resolve;
+    this.reject = reject;
+  }
+}
 
 //TODO 数据库自动种子
